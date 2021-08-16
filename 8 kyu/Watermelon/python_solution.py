8ce5a048d37448b2036079f90ab2d1a56948bdd5
Elad <75241164+Eladtopaz@users.noreply.github.com>
2021-08-16 11:35:28 +0300

    Update python_solution.py

--- 8 kyu/Watermelon/python_solution.py
+++ 8 kyu/Watermelon/python_solution.py
@@ -1,9 +1,2 @@
 def divide(weight):
-    if weight % 2 != 0:
-        return False
-    
-    for i in range(1, weight):
-        if i % 2 == 0 and (weight - i) % 2 == 0:
-            return True
-        
-    return False
+    return weight > 2 and weight % 2 == 0
