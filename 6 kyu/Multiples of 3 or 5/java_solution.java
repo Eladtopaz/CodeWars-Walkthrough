be701e59a9b1100e218e4e58a448fa39d806506c
Elad <75241164+Eladtopaz@users.noreply.github.com>
2021-08-18 09:53:58 +0300

    Create java_solution.java

---
+++ 6 kyu/Multiples of 3 or 5/java_solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+
+  public int solution(int number) {
+    int sum = 0;
+    for(int i = 1; i < number; i++)
+    {
+      if(i % 3 == 0 || i % 5 == 0)
+      {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+}
