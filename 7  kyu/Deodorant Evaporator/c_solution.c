cd75bc5da00b325445456ea3a842a64fef02e9d6
Elad <75241164+Eladtopaz@users.noreply.github.com>
2021-08-22 10:15:18 +0300

    Create c_solution.c

---
+++ 7  kyu/Deodorant Evaporator/c_solution.c
@@ -0,0 +1,10 @@
+int evaporator(double content, double evap_per_day, double threshold) {
+    int i = 0;
+    double res = (threshold / 100) * content;
+    while(content > res)
+    {
+        content = content - (content * (evap_per_day / 100));
+        i += 1;
+    }
+    return i;
+}
